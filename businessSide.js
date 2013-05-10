58a848e461c34dbc6bbc8fcf8f9ecde4be4065f7
Ofer <oferrotem@gmail.com>
2013-05-10 21:16:04 +0300

    working on json

--- businessSide.js
+++ businessSide.js
@@ -2,3 +2,15 @@ function updateBtcPrice(){
     document.getElementById('btc').innerHTML = '1';
 }
 updateBtcPrice();
+
+$.getJSON('https://data.mtgox.com/api/2/BTCUSD/money/ticker', function(data) {
+    //alert(data);
+    var obj = jQuery.parseJSON(data);
+    
+    for (var key in obj) {
+        if (obj.hasOwnProperty(key)) {
+            console.log(obj[key]);
+        }
+    }
+//    alert(obj);
+});
